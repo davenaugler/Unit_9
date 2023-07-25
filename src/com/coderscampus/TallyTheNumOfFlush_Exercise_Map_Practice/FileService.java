b87f90d78b0057285775ea08d86b8a4f4085763c
Dave Naugler <davenaugler@gmail.com>
2023-07-24 18:01:37 -0600

    Creating FileService - needing to re-think it's structure. Evening commit

--- src/com/coderscampus/TallyTheNumOfFlush_Exercise_Map_Practice/FileService.java
+++ src/com/coderscampus/TallyTheNumOfFlush_Exercise_Map_Practice/FileService.java
@@ -12,44 +12,36 @@ public class FileService {
 	// Key = Player Name
 	// Value = Winning Hand type. In this case a Flush.
 	// Does a Royal Flush count towards the count?
-	public Map<Player, String> readFile(String fileName) {
+	public Map<Player, String> readFile1(String fileName) {
 		Map<Player, String> hand = new HashMap<>();
 		boolean isFirstLine = true;
-//		int columnNumberInCSV = 1;
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			String line;
-			// Skips first line on .csv (aka header on .csv)
-			while ((line = br.readLine()) != null) {
+			while((line = br.readLine()) != null) {
 				if (isFirstLine) {
 					isFirstLine = false;
 					continue;
 				}
 				
-				// Split the line by CSV delimiter
 				String[] playerInfo = line.split(",");
+				
 				String player = playerInfo[0];
 				String winningHand = playerInfo[1];
-				Player player = new Player(player, winningHand);
 				
+				Player player1 = new Player(player1, winningHand);
 				
-				
-//				for (Player )
-				System.out.println(valuesInCSV.toString());
+				System.out.println(playerInfo);
 			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 		return hand;
 		
 	}
 	
-	
+
 	
 }
