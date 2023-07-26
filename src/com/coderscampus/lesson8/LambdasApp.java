b27052aee071d6428ab766713b0bef794e93308a
Dave Naugler <davenaugler@gmail.com>
2023-07-26 17:09:35 -0600

    Initial commit for Lesson 8 - Lambdas

---
+++ src/com/coderscampus/lesson8/LambdasApp.java
@@ -0,0 +1,48 @@
+package com.coderscampus.lesson8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdasApp {
+	
+	public static void main(String[] args) {
+		
+		List<String> names = new ArrayList<>();
+		names.add("Dave Naugler");
+		names.add("John Doe");
+		names.add("Jane Doe");
+		names.add("Elon Musk");
+		names.add("Frank Beard");
+		
+		// Sort in Acceding order
+		Collections.sort(names);
+		printArrayListToConsole(names);
+		
+		Collections.sort(names, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o2.compareTo(o1);
+			}
+			
+		});
+		printArrayListToConsole(names);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private static void printArrayListToConsole(List<String> names) {
+		for (String name : names) {
+			System.out.println(name);
+		}
+		System.out.println("");
+	}
+}
